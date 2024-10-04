c9c5a40fe4fdd2f641e1016b12e311156c86740f
Albalad <alvaro.albalad@gmail.com>
2024-10-04 18:09:52 +0000

    primera parte

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronombre = ['la', 'el'];
+let adjetivo = ['mayor', 'intoxicado'];
+let nombre = ['porqueria', 'mapache'];
+let d=(".com");
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for(let i = 0; i < pronombre.length; i++){
+  let a = pronombre[i];
+  for(let j = 0; j < adjetivo.length; j++){
+    let b = adjetivo[j];
+    for(let k = 0; k < nombre.length; k++){
+      let c = nombre[k];
+      console.log(a+b+c+d);
+    }
+  }
+}
