88d94e964121ae976faf5f0286fe2a45ac14d4fa
Albalad <alvaro.albalad@gmail.com>
2024-10-07 10:25:52 +0000

    extra2 semicompleto

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 let pronombre = ["la", "el", "los", "quien"];
 let adjetivo = ["mayor", "intoxicado", "perfecto", "genial"];
-let nombre = ["porqueria", "mapache", "comicom", "trenes"];
+let nombre = ["porqueria", "mapache", "colacom", "trenes"];
 let extension = ["com", "net", "es", "io"];
 
 console.log(nombre[2].slice(-3));
@@ -14,15 +14,17 @@ for (let i = 0; i < pronombre.length; i++) {
     for (let k = 0; k < nombre.length; k++) {
       let c = nombre[k];
 
-      if (c.slice(-2) === extension.find(i => i === c.slice(-2))) {
+      if (c.slice(-2) === extension.find(o => o === c.slice(-2))) {
         let r = "." + c.slice(-2);
         let p = c.replace(c.slice(-2), r);
         console.log(a + b + p);
 
-      } else if (c.slice(-3) === extension.find(i => i === c.slice(-3))) {
+      } else if (c.slice(-3) === extension.find(z => z === c.slice(-3))) {
           let s = "." + c.slice(-3);
           let q = c.replace(c.slice(-3), s);
           console.log(a + b + q);
+
+          //En vez de sustituir las tres últimas, meter un . justo delante
         
       } else {
         for (let l = 0; l < extension.length; l++) {
